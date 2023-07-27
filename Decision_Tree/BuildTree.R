62660f2b1554492e064f81c462edc7d9ed744c46
Kieran Jaggernauth <kieranjager@gmail.com>
2023-07-27 12:02:59 -0500

    Added over/undersampling

--- Decision_Tree/BuildTree.R
+++ Decision_Tree/BuildTree.R
@@ -4,21 +4,38 @@ library(rpart)
 library(rpart.plot)
 library(caTools)
 library(pROC)
+library(ROSE)
 
 # Load cleaned data
 data <- read.csv('./cleanedData.csv', stringsAsFactors = T)
+data$lead <- as.factor(data$lead)
 
 
 # Split into test and training data
 set.seed(1)
-sample <- sample.split(Y=data, SplitRatio = 0.8)
+sample <- sample.split(Y=data$lead, SplitRatio = 0.8)
 train <- data[sample,]
 test <- data[!sample,]
+
+table(train$lead)
+
+# Over/Under Sampling
+# To get balanced values
+# under -> N = 560
+# over -> N = 1220
+# both -> N = 600
+train_adjusted <- ovun.sample(lead ~ ., 
+                          data=train, 
+                          method="over", 
+                          seed = 1, 
+                          N=1220)$data
+
+table(train_adjusted$lead)
 
 ## Build Model
 DTmodel <- rpart(
   lead ~ agerange + job + marital + education + balance + deposit,
-  data = train,
+  data = train_adjusted,
   method = "class",
   minsplit = 50, # Min records to split
   minbucket = 30, # Min records at a node
